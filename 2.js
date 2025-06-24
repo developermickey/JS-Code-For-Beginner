b9315e25042d61a6e744d0417f6623b852987588
mickeysrishti <Mukeshpathak345@gmail.com>
2025-06-24 20:47:07 +0530

    2nd q

---
+++ 2.js
@@ -0,0 +1,11 @@
+// Create a function reverseWords(sentence) that:
+//     Takes a string sentence.
+//     Returns a new string where the order of words is reversed.
+//     Example: "Hello world" → "world Hello".
+// Approach: Think of a way to convert the string into an array of words.
+
+function reverseWords(sentence) {
+     
+
+
+}
