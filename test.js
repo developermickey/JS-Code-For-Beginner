e6d4b4e07f0bebda13a4d847bc723ae6da9a2308
mickeysrishti <Mukeshpathak345@gmail.com>
2025-06-24 20:40:05 +0530

    code added

---
+++ test.js
@@ -0,0 +1,16 @@
+// Write a function convertTemperature(temp, unit) that:
+//   Converts Celsius to Fahrenheit and vice versa.
+//   Returns the converted temperature.
+//   Example: convertTemperature(100, "C") → 212F.
+
+function CelToFah(celsius) {
+    return celsius * 9 / 5 + 32;
+  }
+  
+  function FahToCel(fahrenheit) {
+    return (fahrenheit - 32) * 5 / 9;
+  }
+  
+  function convertTemperature(temp, unit) {
+     
+  }
